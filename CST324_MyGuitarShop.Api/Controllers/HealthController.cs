0538980e43fc372f73a92cbe5891c3080e7b611b
Arthur Spears <arthur.spears@oit.edu>
2025-10-25 16:33:57 -0700

    Health controller set

---
+++ CST324_MyGuitarShop.Api/Controllers/HealthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyGuitarShop.Data.Ado.Factories;
+
+namespace CST324_MyGuitarShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController(
+        ILogger<HealthController> logger,
+        SqlConnectionFactory sqlConnectionFactory) 
+        : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok("Healthy");
+            }
+            catch(Exception)
+            {
+                logger.LogWarning("Health check failed unreasonably");
+                return StatusCode(503, "Unhealthy");
+            }
+        }
+
+        [HttpGet("db")]
+        public IActionResult GetDbHealth()
+        {
+            try
+            {
+                using var connection = sqlConnectionFactory.OpenSqlConnection();
+                return Ok(new{Message="Connection Successful!", connection.Database});
+            }
+            catch (Exception)
+            {
+                logger.LogCritical("Database health check failed");
+
+                return StatusCode(503, "Database Unhealthy");
+            }
+        }
+    }
+}
